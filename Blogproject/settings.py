5a63ac3bc0d5c0ea51d3ef42c06bdd445135e6a3
Utsab Gayawali <utsavgayawali@gmail.com>
2025-05-31 16:59:32 +0545

    final-call

--- Blogproject/settings.py
+++ Blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s32y#n@@t_k0cbmh4w_!n3+n+=!2t$tj^yc&gzxad*p$9vv3_y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mountain-coder-blog.onrender.com']
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
    
 ]
 
